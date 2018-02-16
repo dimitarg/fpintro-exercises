c34fa79b594feca25355ee54dc770fdab270caf9
Dimitar Georgiev <dimitar.georgiev@novarto.com>
2018-02-16 12:48:05 +0200

    Pure treatment of side effects

--- src/main/java/org/example/chapter6/Strictness.java
+++ src/main/java/org/example/chapter6/Strictness.java
@@ -1,8 +1,6 @@
 package org.example.chapter6;
 
-import fj.Effect;
-import fj.F;
-import fj.F0;
+import fj.*;
 import fj.data.List;
 import fj.data.Stream;
 import fj.function.Effect0;
@@ -10,6 +8,7 @@ import fj.function.Effect1;
 import fj.function.Try0;
 import fj.function.TryEffect0;
 
+import static fj.P.lazy;
 import static org.example.Util.println;
 
 public class Strictness
@@ -45,6 +44,27 @@ public class Strictness
 
         Stream<Integer> xs = integers.filter(i -> i % 3 == 0).filter(i -> i > 200).take(5);
         System.out.println(xs.toList());
+
+
+
+        F1W<Integer, Integer> ff = F1W.lift(Strictness::bar);
+        F1W<Integer, Integer> mapped = ff.map(a -> a + 3);
+        System.out.println(mapped.f(2));
+
+        P1<Integer> foo = lazy(Strictness::foo);
+        P1<Integer> twice = foo.map(a -> a * 2);
+        Integer res = twice.f();
+        System.out.println(res);
+
+        P1<Integer> memo = foo.hardMemo();
+        println(memo.f());
+        println(memo.f());
+
+        P1<Integer> twice_ = memo.map(a -> a * 2);
+        println(twice_.f());
+        println(twice_.f());
+
+        System.out.println();
 
     }
 
@@ -86,6 +106,11 @@ public class Strictness
         return cond.f() ? ifExpr.f() : elExpr.f();
     }
 
+    public static int bar(int x)
+    {
+        return x*2;
+    }
+
 
     public static <R, A> A withResource(F0<R> acquire, F<R, A> code, Effect1<R> e)
     {
@@ -99,6 +124,12 @@ public class Strictness
         {
             e.f(res);
         }
+    }
+
+    public static int foo()
+    {
+        System.out.println("computing");
+        return 42;
     }
 
 }
---
+++ src/main/java/org/example/chapter6/TryExample.java
@@ -0,0 +1,107 @@
+package org.example.chapter6;
+
+import fj.P1;
+import fj.Try;
+import fj.data.List;
+import fj.data.Validation;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class TryExample
+{
+
+    public static void main(String[] args)
+    {
+        P1<Validation<Exception, String>> fileContents = getFileContents("/farenheit.txt");
+
+        P1<Validation<Exception, List<Double>>> toCelsiusProgram = fileContents.map(v -> v.map(contents -> farenheit(contents)));
+
+        // end of the world
+        Validation<Exception, List<Double>> celsiusV = toCelsiusProgram.f(); // toCelsiusProgram.run()
+        System.out.println(celsiusV);
+
+    }
+
+
+
+    static List<Double> farenheit(String fileContents)
+    {
+        List<Double> farenheits = doubleRows(nonEmptyRows(fileContents));
+        return farenheits.map(a -> toCelsius(a));
+    }
+
+
+
+    static double toCelsius(double farenheit)
+    {
+        return ((farenheit - 32) * 5) / 9;
+    }
+
+
+
+    static List<String> nonEmptyRows(String contents)
+    {
+        String[] xs = contents.split("\n");
+        List<String> rows = List.arrayList(xs);
+        return rows.map(String::trim).filter(x -> !x.isEmpty());
+    }
+
+    static List<Double> doubleRows(List<String> rows)
+    {
+        List<Validation<NumberFormatException, Double>> xs = rows.map(x -> {
+
+            P1<Validation<NumberFormatException, Double>> res = Try.f(() -> Double.parseDouble(x));
+            return res.f();
+        });
+
+        List<Double> res = xs.filter(a -> a.isSuccess()).map(a -> a.success());
+        return res;
+    }
+
+
+
+    static P1<Validation<Exception, String>> getFileContents(String pathString)
+    {
+        P1<Validation<Exception, Path>> getPath = Try.f(() -> unsafeToPath(pathString));
+
+        P1<Validation<Exception, String>> res = getPath.map(pathV -> {
+
+            Validation<Exception, String> getContents = pathV.bind(path -> Try.f(() -> unsafeReadFile(path)).f());
+            return getContents;
+
+        });
+
+        return res;
+    }
+
+
+    //side-effecting procedures
+
+    private static String unsafeReadFile(Path p) throws Exception
+    {
+        System.out.println("entering unsafeReadFile");
+        return new String(
+                Files.readAllBytes(p), StandardCharsets.UTF_8
+        );
+    }
+
+    private static Path unsafeToPath(String cpPath) throws Exception
+    {
+        System.out.println("entering unsafeToPath");
+        URL res = TryExample.class.getResource(cpPath);
+
+        if(res == null)
+        {
+            throw new IllegalArgumentException("no such resource: " + cpPath);
+        }
+
+        return Paths.get(res.toURI());
+    }
+
+
+}
